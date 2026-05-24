75902bb4f4fc363aa48dabf99b6a89cc4488ff15
Higor Valuar <higorvb2001@gmail.com>
2026-05-23 23:19:13 -0300

    chore: cria projeto base Spring Boot com Maven

---
+++ src/main/java/br/unitins/techflow/TechflowApiApplication.java
@@ -0,0 +1,13 @@
+package br.unitins.techflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechflowApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechflowApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/unitins/techflow/TechflowApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.unitins.techflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechflowApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
